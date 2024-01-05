b8bb5c3e34152f2366c51a6263d1b5a3e3ec8395
rochelleaguila <143100232+rochelleaguila@users.noreply.github.com>
2024-01-05 06:33:40 +0000

    all tests passed

--- src/app.py
+++ src/app.py
@@ -26,17 +26,78 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def handle_get_all_members():
 
-    # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+        
+    if len(members) == 0:
+        # If there are no members in the database, return 404 Not Found
+        raise APIException('No members found', status_code=404)
+
+    return jsonify(members), 200
 
 
-    return jsonify(response_body), 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def handle_get_member(id):
+
+    member = jackson_family.get_member(id)
+    if member is None:
+        raise APIException('Member not found', status_code=404)
+
+    return jsonify(member), 200
+
+
+@app.route('/member', methods=['POST'])
+def handle_add_member():
+    member = request.get_json()
+
+    if member is None:
+        raise APIException("You need to specify the request body as a JSON object", status_code=400)
+    if 'first_name' not in member:
+        raise APIException('You need to specify the first name', status_code=400)
+    if 'age' not in member:
+        raise APIException('You need to specify the age', status_code=400)
+    if 'lucky_numbers' not in member:
+        raise APIException('You need to specify the lucky numbers', status_code=400)
+
+    jackson_family.add_member(member)
+
+    return jsonify(member), 200
+
+@app.route('/member/<int:id>', methods=['PUT'])
+def handle_update_member(id):
+    body = request.get_json()
+
+    if body is None:
+        raise APIException("You need to specify the request body as a JSON object", status_code=400)
+    if 'first_name' not in body:
+        raise APIException('You need to specify the first name', status_code=400)
+    if 'age' not in body:
+        raise APIException('You need to specify the age', status_code=400)
+    if 'lucky_numbers' not in body:
+        raise APIException('You need to specify the lucky numbers', status_code=400)
+
+    member = jackson_family.get_member(id)
+    if member is None:
+        raise APIException('Member not found', status_code=404)
+
+    member.first_name = body['first_name']
+    member.age = body['age']
+    member.lucky_numbers = body['lucky_numbers']
+
+    return jsonify(member), 200
+    
+@app.route('/member/<int:id>', methods=['DELETE'])
+def handle_delete_member(id):
+    member = jackson_family.get_member(id)
+    if member is None:
+        raise APIException('Member not found', status_code=404)
+
+    jackson_family.delete_member(id)
+    return jsonify({"done": True}), 200
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -13,7 +13,29 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+            {
+                'id': self._generateId(),
+                'first_name': 'John',
+                'last_name': last_name,
+                'age': 33,
+                'lucky_numbers': [7, 13, 22]
+            },
+            {
+                'id': self._generateId(),
+                'first_name': 'Jane',
+                'last_name': last_name,
+                'age': 35,
+                'lucky_numbers': [10, 14, 3]
+            },
+            {
+                'id': self._generateId(),
+                'first_name': 'Jimmy',
+                'last_name': last_name,
+                'age': 5,
+                'lucky_numbers': [1]
+            }
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -21,15 +43,37 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        ## append the member to the list of _members
+        member['last_name'] = self.last_name
+        self._members.append(member)
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        ## loop the list and delete the member with the given id
+
+        for member in self._members: 
+            if member['id'] == id:
+                self._members.remove(member)
+                return True
+        return False
+            
+
+    def update_member(self, id, new_member):
+        # Loop through the list of members
+        for member in self._members:
+            if member['id'] == id:
+                # Replace the member with the given ID
+                self._members[id] = new_member
+        return None
+
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        ## loop all the members and return the one with the given id
+        for member in self._members:
+            if member['id'] == id:
+                return member
+        return None
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
